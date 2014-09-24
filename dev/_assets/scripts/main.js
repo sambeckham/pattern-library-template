15bcbb04810afc9c0288557c3d8ac4edbee4ba8a
samdbeckham <sam@drummondcentral.co.uk>
2014-09-24 09:56:20 +0100

    Require loaded in fully now, on to the build process

--- dev/_assets/scripts/main.js
+++ dev/_assets/scripts/main.js
@@ -1,1 +1,23 @@
-alert('sausage');
+/*global requirejs:false */
+'use strict';
+
+requirejs.config({
+    baseUrl: '/scripts/plugins',
+    paths: {
+        jquery: '../../bower_components/jquery/dist/jquery'
+    }
+});
+
+/*
+    00. Test Code
+*/
+
+
+// Show Me The Code
+// ----------
+require(['jquery', 'testPlugin'], function($){
+    // A test to check jQuery is running
+    console.log('jQuery test', $.now());
+    // A test to check the modules get loaded correctly
+    $('body').logWidth();
+});
