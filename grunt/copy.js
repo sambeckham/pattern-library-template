1e434588f2ad5766622507e73299e387c7bebc05
willforsyth <will.forsyth@drummondcentral.co.uk>
2015-01-05 14:31:16 +0000

    add in ie8 support for svg

--- grunt/copy.js
+++ grunt/copy.js
@@ -13,7 +13,16 @@ module.exports = {
             dot: true,
             cwd: 'dev/_assets/',
             dest: '.tmp',
-            src: 'img/{,*/}*.{jpg,png,jpeg,gif}'
+            src: 'imgs/{,*/}*.{png,jpg,jpeg,gif,svg}'
+        }]
+    },
+     dist:{
+        files: [{
+            expand: true,
+            dot: true,
+            cwd: 'dev/_assets/',
+            dest: 'web',
+            src: ['imgs/{,*/}*.{png,jpg,jpeg,gif,svg}']
         }]
     }
 };
