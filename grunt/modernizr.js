50d2ed44023695fd440c0d833a5b76185bce9cd5
willforsyth <will.forsyth@drummondcentral.co.uk>
2015-01-22 16:48:55 +0000

    hotfix svg for everybody build and bump version no

--- grunt/modernizr.js
+++ grunt/modernizr.js
@@ -2,6 +2,13 @@ module.exports = {
     dist: {
         devFile: 'bower_components/modernizr/modernizr.js',
         outputFile: 'web/scripts/vendor/modernizr.js',
+        extra: {
+            'shiv' : true,
+            'printshiv' : false,
+            'load' : true,
+            'mq' : false,
+            'cssclasses' : true
+        },
         files: {
             src: [
                 'web/scripts/{,*/}*.js',
