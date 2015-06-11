174db7b687dc62ce9cfbfa5f114917188553420c
Will Forsyth <will.forsyth@drummondcentral.co.uk>
2015-06-11 13:26:24 +0100

    start to look a some js for hiding and showing the menu
    • Toolbar icon -- not to sure about it yet
    • Show and hide working get some animation in there

--- dev/_assets/scripts/components/toolbar.js
+++ dev/_assets/scripts/components/toolbar.js
@@ -1,8 +1,18 @@
 /*global $ */
 $.fn.toolbar = function (options) {
     'use strict';
+    $('.toolbar__show').on('click', function(e){
+    	e.preventDefault();
+    	$('.toolbar').toggleClass('hide');
+    	if($('.toolbar').hasClass('hide')){
+    		$('body').css('margin-top', '0');
+    	} else{
+    		$('body').css('margin-top', '3em');
+    	}
+
+    });
     console.log('This is the toolbar');
-    $('.sizes').on('click', '[data-width]', function() {
+    $('.toolbar').on('click', '[data-width]', function() {
 	    var width = $(this).data('width');
 	    $('body').animate({'width': width});
 	});
