9e2a97940d09c9a0fa2075482587c0bf3ed412c6
Will Forsyth <will.forsyth@drummondcentral.co.uk>
2015-06-12 10:13:16 +0100

    add some animations to toolbar
    • add animation to the toolbar show and hide
    • add home button to tool bar
    • build out to test

--- dev/_assets/scripts/components/toolbar.js
+++ dev/_assets/scripts/components/toolbar.js
@@ -3,17 +3,11 @@ $.fn.toolbar = function (options) {
     'use strict';
     $('.toolbar__show').on('click', function(e){
     	e.preventDefault();
-    	$('.toolbar').toggleClass('hide');
-		var body = $('body');
-    	if($(body).hasClass('add_margin')){
-    		$(body).removeClass('add_margin');
-    	} else{
-    		$(body).addClass('add_margin');
-    	}
+    	$('.toolbar__inner').toggleClass('show');
     });
     console.log('This is the toolbar');
     $('.toolbar').on('click', '[data-width]', function() {
 	    var width = $(this).data('width');
-	    $('body').animate({'width': width});
+	    $('.pattern__wrapper ').animate({'width': width});
 	});
 };
